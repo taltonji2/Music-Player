b94a6a1e83ba04d409f0f968be2a6937349140d7
sskibin22 <50159952+sskibin22@users.noreply.github.com>
2022-02-06 14:56:33 -0500

    Set up UI for song library program

---
+++ src/app/SongLib.java
@@ -0,0 +1,37 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import view.SongListController;
+ 
+public class SongLib extends Application {
+
+    @Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		// set up FXML loader
+		FXMLLoader loader = new FXMLLoader();   
+		loader.setLocation(
+				getClass().getResource("/view/SongList.fxml"));
+		
+		// load the fxml
+		AnchorPane root = (AnchorPane)loader.load();
+
+        // get the controller
+		SongListController slistController = loader.getController();
+		
+
+		Scene scene = new Scene(root, 560, 500);
+		primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+		primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+    }
+}
